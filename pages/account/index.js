424c842e715ef5c7cea66ef994e9346b1ab9f07e
Alan Ng <alan@gecko.rent>
2022-02-10 15:24:43 +1100

    feat: added account route pages

---
+++ pages/account/index.js
@@ -0,0 +1,11 @@
+import AccountForm from "components/AccountForm";
+
+function AccountIndexPage() {
+  return (
+    <div>
+      <AccountForm />
+    </div>
+  );
+}
+
+export default AccountIndexPage;
