40c11867fb6985c8d6104df37b3cc82cd6362c77
Alan Ng <alan@gecko.rent>
2022-02-10 15:41:04 +1100

    chore: updated styling flex for accounts form

--- components/AccountForm/AccountForm.js
+++ components/AccountForm/AccountForm.js
@@ -9,6 +9,7 @@ import {
   useFormControl,
   Wrap,
   WrapItem,
+  Spacer,
 } from "@chakra-ui/react";
 import { chakra, useMultiStyleConfig } from "@chakra-ui/system";
 
@@ -23,56 +24,121 @@ const AccountForm = ({}) => {
   return (
     <>
       <form>
-        <Box display={{ md: "flex" }}>
-          <FormControl id="first-name" isRequired isInvalid>
+        <Box
+          display={{ md: "flex" }}
+          flexDirection={"row"}
+          flexWrap={"wrap"}
+          justifyContent={"center"}
+        >
+          <FormControl
+            id="first-name"
+            isRequired
+            isInvalid
+            w={{ base: "100%", md: "48%" }}
+          >
             <FormLabel>First name</FormLabel>
             <Input placeholder="First Name" />
             <FormErrorMessage>Your First name is invalid</FormErrorMessage>
           </FormControl>
-          <FormControl id="last-name" isRequired isInvalid>
+          <Spacer />
+          <FormControl
+            id="last-name"
+            isRequired
+            isInvalid
+            w={{ base: "100%", md: "48%" }}
+          >
             <FormLabel>Last name</FormLabel>
             <Input placeholder="Last Name" />
             <FormErrorMessage>Your First name is invalid</FormErrorMessage>
           </FormControl>
-          <FormControl id="email" isRequired isInvalid>
+          <Spacer />
+          <FormControl
+            id="email"
+            isRequired
+            isInvalid
+            w={{ base: "100%", md: "48%" }}
+          >
             <FormLabel>Email</FormLabel>
             <Input placeholder="Email" />
             <FormErrorMessage>Your Email is invalid</FormErrorMessage>
           </FormControl>
-          <FormControl id="phone-number" isRequired isInvalid>
+          <Spacer />
+          <FormControl
+            id="phone-number"
+            isRequired
+            isInvalid
+            w={{ base: "100%", md: "48%" }}
+          >
             <FormLabel>Phone Number</FormLabel>
             <Input placeholder="Phone Number" />
             <FormErrorMessage>Your Phone Number is invalid</FormErrorMessage>
           </FormControl>
-          <FormControl id="postcode" isRequired isInvalid>
+          <Spacer />
+          <FormControl
+            id="postcode"
+            isRequired
+            isInvalid
+            w={{ base: "100%", md: "48%" }}
+          >
             <FormLabel>Postcode</FormLabel>
             <Input placeholder="Postcode" />
             <FormErrorMessage>Your Postcode is invalid</FormErrorMessage>
           </FormControl>
-          <FormControl id="state" isRequired isInvalid>
+          <Spacer />
+          <FormControl
+            id="state"
+            isRequired
+            isInvalid
+            w={{ base: "100%", md: "48%" }}
+          >
             <FormLabel>State</FormLabel>
             <Input placeholder="Select state" />
           </FormControl>
-          <FormControl id="company-name" isRequired isInvalid>
+          <Spacer />
+          <FormControl
+            id="company-name"
+            isRequired
+            isInvalid
+            w={{ base: "100%", md: "48%" }}
+          >
             <FormLabel>Company Name</FormLabel>
             <Input placeholder="Company Name" />
             <FormErrorMessage>Your company name is required</FormErrorMessage>
           </FormControl>
-          <FormControl id="abn" isRequired isInvalid>
+          <Spacer />
+          <FormControl
+            id="abn"
+            isRequired
+            isInvalid
+            w={{ base: "100%", md: "48%" }}
+          >
             <FormLabel>ABN</FormLabel>
             <Input placeholder="ABN" />
             <FormErrorMessage>Your ABN is required</FormErrorMessage>
           </FormControl>
-          <FormControl id="hourly-rate" isRequired isInvalid>
+          <Spacer />
+          <FormControl
+            id="hourly-rate"
+            isRequired
+            isInvalid
+            w={{ base: "100%", md: "48%" }}
+          >
             <FormLabel>Hourly Rate</FormLabel>
             <Input placeholder="Hourly Rate" />
             <FormErrorMessage>Your Hourly Rate is required</FormErrorMessage>
           </FormControl>
-          <FormControl id="dob" isRequired isInvalid>
+          <Spacer />
+          <FormControl
+            id="dob"
+            isRequired
+            isInvalid
+            w={{ base: "100%", md: "48%" }}
+          >
             <FormLabel>Date of Birth</FormLabel>
             <Input placeholder="Date of Birth" />
             <FormErrorMessage>Your Date of Birth is required</FormErrorMessage>
           </FormControl>
+          <Spacer />
         </Box>
       </form>
     </>
