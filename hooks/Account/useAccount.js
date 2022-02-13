0b093d89b5709858c17573ca962ec3537bfc43b1
Alan Ng <alan@gecko.rent>
2022-02-13 23:57:36 +1100

    chore: initialise default values

--- hooks/Account/useAccount.js
+++ hooks/Account/useAccount.js
@@ -1,5 +1,8 @@
+import { useEffect } from "react";
+
 export default function useAccount() {
   const DEFAULT_VALUE = {
+    initialised: true,
     firstName: "Bruce",
     lastName: "Banner",
     email: "bruce.banner@marvel.com",
@@ -9,6 +12,18 @@ export default function useAccount() {
     companyName: "Marvel",
     abn: "12123123123",
     rate: 379,
-    dob: "",
+    dob: new Date("May 21, 1970"),
   };
+
+  useEffect(() => {
+    const initialised = JSON.parse(localStorage.getItem("initialised"));
+
+    console.log("initialised", initialised);
+
+    if (!initialised) {
+      Object.keys(DEFAULT_VALUE).forEach((key) => {
+        localStorage.setItem(key, JSON.stringify(DEFAULT_VALUE[key]));
+      });
+    }
+  }, [DEFAULT_VALUE]);
 }
